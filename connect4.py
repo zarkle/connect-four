e38717b7306e27ed87b420b561e02918da67e370
Beverly Pham <beverly.pham@gmail.com>
2019-04-05 15:51:59 -0700

    each player takes a turn entering in a column

--- connect4.py
+++ connect4.py
@@ -6,4 +6,21 @@ def create_board():
     return board
 
 board = create_board()
-print(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 input
+    if turn == 0:
+        selection = int(input('Player 1 make your selection (0-6): '))
+        if not 0 <= selection < 7:
+            continue
+
+    # Ask for Player 2 input
+    else:
+        selection = int(input('Player 2 make your selection (0-6): '))
+        if not 0 <= selection < 7:
+            continue
+
+    turn += 1
+    turn = turn % 2  # to alternate between player 1 and player 2
