ff8c3078002f4f84d543129cf6e1c617c99f186d
Beverly Pham <beverly.pham@gmail.com>
2019-04-05 15:59:49 -0700

    write is valid column function

--- connect4.py
+++ connect4.py
@@ -2,8 +2,25 @@ import numpy as np
 
 
 def create_board():
-    board = np.zeros((6,7))
+    """Create x 6 x 7 Connect Four board."""
+    board = np.zeros((6, 7))
     return board
+
+
+def drop_piece():
+    """Entered input will drop a piece into Connect Four board."""
+    pass
+
+
+def is_valid_column(board, column):
+    """Check to see if entered column has an empty spot for piece. Check this by making sure the top row of the board (5th row) contains 0 for the entered column."""
+    return board[5][column] == 0
+
+
+def get_next_open_row():
+    """."""
+    pass
+
 
 board = create_board()
 game_over = False
@@ -12,13 +29,13 @@ turn = 0
 while not game_over:
     # Ask for Player 1 input
     if turn == 0:
-        selection = int(input('Player 1 make your selection (0-6): '))
+        column = int(input('Player 1 make your column selection (0-6): '))
         if not 0 <= selection < 7:
             continue
 
     # Ask for Player 2 input
     else:
-        selection = int(input('Player 2 make your selection (0-6): '))
+        column = int(input('Player 2 make your column selection (0-6): '))
         if not 0 <= selection < 7:
             continue
 
